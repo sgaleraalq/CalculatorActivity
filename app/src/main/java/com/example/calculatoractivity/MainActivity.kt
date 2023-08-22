21c85ec566cfd4055e7943a0204bb70287f8fcd9
sgaleraalq <sgaleraalq@gmail.com>
2023-08-22 20:59:00 +0200

    Calculator Layout done

--- app/src/main/java/com/example/calculatoractivity/MainActivity.kt
+++ app/src/main/java/com/example/calculatoractivity/MainActivity.kt
@@ -2,45 +2,10 @@ package com.example.calculatoractivity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.calculatoractivity.ui.theme.CalculatorActivityTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CalculatorActivityTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculatorActivityTheme {
-        Greeting("Android")
+        setContentView(R.layout.calculator_layout)
     }
 }
