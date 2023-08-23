a155d037934ac7e2661cfab7fe8eb70dbb13dab7
sgaleraalq <sgaleraalq@gmail.com>
2023-08-23 21:52:19 +0200

    Some updates

--- app/src/main/java/com/example/calculatoractivity/MainActivity.kt
+++ app/src/main/java/com/example/calculatoractivity/MainActivity.kt
@@ -11,61 +11,63 @@ class MainActivity : ComponentActivity() {
     private lateinit var tvOperation: TextView
     private lateinit var tvResult: TextView
     private lateinit var btnEqual: Button
+    private lateinit var btnClear: Button
+
+    private var canAddOperator:Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calculator_layout)
         initComponents()
-        initUI()
+        initListeners()
     }
-
-    // Initalization functions
-    
-    private fun initUI(){
-        setResultTextView()
-    }
-
     private fun initComponents(){
         tvOperation = findViewById(R.id.tvOperation)
         tvResult = findViewById(R.id.tvResult)
         btnEqual = findViewById(R.id.btnEqual)
+        btnClear = findViewById(R.id.btnClear)
     }
 
     private fun initListeners(){
-        btnEqual.setOnClickListener { calculate() }
+        btnEqual.setOnClickListener { equalCalculate() }
+        btnClear.setOnClickListener { clearEverything() }
     }
 
-    // Calculator functions
-
-    // Private functions
-    
-    private fun setResultTextView(){
-        tvResult.text = "0"
+    fun pressNumber(btn:View){
+        setResult()
+        if ( btn is Button && btn.text == "0" && tvOperation.text == "" ){ return }
+        if ( btn is Button ){
+            tvOperation.append(btn.text)
+            canAddOperator = true
+        }
+    }
+    fun pressOperator(btn:View){
+        if(!canAddOperator){ return }
+        if (btn is Button){
+            tvOperation.append(btn.text)
+            canAddOperator = false
+        }
     }
 
-    private fun clearEverything(){
+
+    fun clearEverything() {
         tvOperation.text = ""
         tvResult.text = ""
     }
 
-    private fun calculate(): String {
-        var result = 0
-        return result.toString()
-    }
-
-
-    // Non-private functions --> Accessible from layout
-    
-    fun pressNumber(view:View){
-        if (view is Button){
-            tvOperation.text = view.text
+    private fun setResult() {
+        if (tvOperation.text.contains("X") || tvOperation.text.contains("+") ||
+                    tvOperation.text.contains("-") || tvOperation.text.contains("/")) {
+            calculate()
         }
+        else{ return }
     }
 
-    fun pressOperator(view:View){
-        if (view is Button && tvOperation.text.isNotEmpty()){
-            tvOperation.text = view.text
-        }
-    }
+    private fun calculate(){
 
+    }
+    private fun equalCalculate(){
+
+    }
 }
